77e7d54ce8f555215a3754d249df70e8efb0acda
Arkirka <denis.vorobyov.00@gmail.com>
2023-05-28 22:00:26 +0300

    fix: jwt auth

---
+++ src/main/java/ru/vorobyov/socialmediaapi/dto/authorization/LoginResponse.java
@@ -0,0 +1,37 @@
+package ru.vorobyov.socialmediaapi.dto.authorization;
+
+
+public class LoginResponse {
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    private final String type = "Bearer";
+    private String accessToken;
+    private String refreshToken;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
